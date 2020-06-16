a272bf96f99113821bec67739075d8e56da69414
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2020-06-16 12:54:47 +0700

    db update

--- inventaris_obat.sql
+++ inventaris_obat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2020 at 07:46 AM
+-- Generation Time: Jun 16, 2020 at 07:54 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
